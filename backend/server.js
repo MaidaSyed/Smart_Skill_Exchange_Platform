714ab97c661f9ae66678f36affc901d00209e37f
MaidaSyed <maidasyed6@gmail.com>
2026-04-25 20:08:50 +0500

    fix port for deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ const adminRoutes = require("./routes/adminRoutes.js");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use(async (req, res, next) => {
@@ -63,6 +65,8 @@ app.use(walletRoutes);
 app.use(reviewsRoutes);
 app.use(adminRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
